313278784f657c827dc5f157e0eb6fdad575732c
sakibb7 <sakibb076@gmail.com>
2024-01-13 23:11:26 +0600

    added midleware and login registration feature

---
+++ routes.ts
@@ -0,0 +1,33 @@
+/**
+ * An array of routes that are accessible to the public
+ * These routes do not require authentication
+ * @type {string[]}
+ */
+export const publicRoutes = [
+    "/",
+]
+
+/**
+ * An array of routes that are used for authentication
+ * These routes will redirect to login page
+ * @type {string[]}
+ */
+export const authRoutes = [
+    "/auth/login", 
+    "/auth/registration", 
+]
+
+/**
+ * The prefix for API authentication routes
+ * Routes that start with this prefix are used for API authentication process
+ * @type {string}
+ */
+
+export const apiAuthPrefix = "/api/auth";
+
+/**
+ * default redirect path after login
+ * @type {string}
+ */
+
+export const DEFAULT_LOGIN_REDIRECT = "/settings";
